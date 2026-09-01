f672cf2252839a829ef56ebec144a5702bccfba0
Aqib Ali <aqibali007302@gmail.com>
2026-09-01 17:30:57 +0500

    fix: change exec to direct command for starting Gunicorn in start.sh

--- blog_backend/start.sh
+++ blog_backend/start.sh
@@ -11,7 +11,7 @@ if [ "${USE_S3:-False}" != "True" ]; then
 fi
 
 # Start Gunicorn
-exec gunicorn config.wsgi:application \
+gunicorn config.wsgi:application \
     --bind "0.0.0.0:${PORT:-8000}" \
     --workers 3 \
     --timeout 120
